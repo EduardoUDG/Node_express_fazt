36809d588d6150f2894acadde147bab8bbf430e6
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2021-09-08 14:45:27 -0500

    peticiones get

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ app.get('/', (req,res) => {
     res.send('Hola mundo');
 });
 
+app.get('/about', (req,res) => {
+    res.send('Soy about');
+});
+
+app.get('/contact', (req,res) => {
+    res.send('Form contact');
+});
+
+app.get('/test', (req,res) => {
+    res.send('<h1>TEST</h1>');
+});
+
+
 
 app.listen( port, (req,res) => {
     console.log(`Servidor corriendo en puerto ${port}`)
