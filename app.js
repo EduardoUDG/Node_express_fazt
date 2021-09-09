fbfb505af14d1fa84ed79fe9dbda09e8e3b4a687
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2021-09-08 20:44:06 -0500

    correciones

--- app.js
+++ app.js
@@ -9,45 +9,45 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // app.all('/user', (req, res, next) => {
-    //     console.log('Por aqui paso')
-    //     next();
-    // });
+//     console.log('Por aqui paso')
+//     next();
+// });
     
-    app.get('/user', (req,res) => {
-        res.json({
-            msg: `GET - usuarios`,
-        });
+app.get('/user', (req,res) => {
+    res.json({
+        msg: `GET - usuarios`,
     });
-    
-    app.post('/user', (req,res) => {
-        const { ...data } = req.body;
-        res.json({
-            msg: `POST - usuario creado`,
-            data
-        });
+});
+
+app.post('/user', (req,res) => {
+    const { ...data } = req.body;
+    res.json({
+        msg: `POST - usuario creado`,
+        data
     });
-    
-    app.put('/user/:id', (req,res) => {
-        const id = req.params.id;
-        res.json({
-            msg: `PUT - usuario ${id} actualizado`,
-            id
-        });
+});
+
+app.put('/user/:id', (req,res) => {
+    const id = req.params.id;
+    res.json({
+        msg: `PUT - usuario ${id} actualizado`,
+        id
     });
-    
-    app.delete('/user/:id', (req,res) => {
-        const id = req.params.id;
-        res.json({
-            msg: `DELETE - Usuario ${parametros} eliminado`,
-            id
-        });
+});
+
+app.delete('/user/:id', (req,res) => {
+    const id = req.params.id;
+    res.json({
+        msg: `DELETE - Usuario ${parametros} eliminado`,
+        id
     });
-    
-    
-    
-    app.listen( port, (req,res) => {
-        console.log(`Servidor corriendo en puerto ${port}`)
-    });
+});
+
+
+
+app.listen( port, (req,res) => {
+    console.log(`Servidor corriendo en puerto ${port}`)
+});
     
     
 //? Por esto ejecutamos los middlewares 
